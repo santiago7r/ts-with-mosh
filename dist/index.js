078de78da6b53d4db9456d1818e965287aec42bf
Eddy Rhenals <eddy.rhenals7@gmail.com>
2025-04-23 16:50:43 -0500

    TS with Mosh beginning

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let sales = 123432323;
+let course = 'TypeScript';
+let is_publised = true;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let sales: number = 123_432_323;
+let course: string = 'TypeScript';
+let is_publised: boolean = true;
+

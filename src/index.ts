21be62ef2fe9446487a61469f6382a22a4e98ddc
Eddy Rhenals <eddy.rhenals7@gmail.com>
2025-04-23 19:23:55 -0500

    Types in typescript

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,53 @@ let sales: number = 123_432_323;
 let course: string = 'TypeScript';
 let is_publised: boolean = true;
 
+
+// Union Type
+
+let unionType: number | string;
+
+
+// Intersection type
+
+type Draggable = {
+    drag: () => void
+};
+
+type Resizable = {
+    resize: () => void
+};
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+    drag: () => {},
+    resize: () => {}
+}
+
+
+// Literal type 
+
+type Quantity = 50 | 150;
+
+let quantity: Quantity = 50;
+
+type Metrics = 'cm' | 'mm';
+
+// Type assertion
+
+let phone = document.getElementById('phone') as HTMLInputElement;
+
+phone.value
+
+// unknown 
+
+function render(document: unknown) {
+
+    // We can Narrowing to use methods that exist
+    if(typeof document === 'string') {
+        document.toLowerCase();
+    }
+}
+
+
+

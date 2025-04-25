71c3a7b57ddd2a67e4da642115fe16106f83e6f5
Eddy Rhenals <eddy.rhenals7@gmail.com>
2025-04-25 13:51:47 -0500

    More methods of TS

--- src/index.ts
+++ src/index.ts
@@ -50,5 +50,89 @@ function render(document: unknown) {
     }
 }
 
+// Index Signatures
+    // We use this to add properties to an object dynamically;
 
+class SeatAssignment {
+    [seatNumber: string]: string; // it can be number, strings, objects or anything.
+}
+
+let seats = new SeatAssignment();
+
+seats.A1 = 'Mosh';
+seats["A2"] = 'John'; // we can access it in multiple ways
+
+//Static Members
+
+class Ride {
+    private static _activeRides: number = 0;
+
+    start() { Ride._activeRides++ };
+    stop() { Ride._activeRides-- };
+
+    static get activeRides() {
+        return Ride._activeRides
+    }
+}
+
+let ride1 = new Ride();
+ride1.start();
+
+let ride2 = new Ride();
+ride2.start();
+
+// Inheritance
+
+class Person {
+    constructor(public firstName: string, public lastName: string) {}
+
+    get fullName() {
+        return this.firstName+' '+this.lastName;
+    }
+
+    walk() {
+        console.log('Walking');
+    }
+}
+
+class Student extends Person {
+    constructor(public studentId: number, firstName: string, lastName: string) {
+        super(firstName, lastName)
+    }
+
+    takeTest() {
+        console.log('Taking a test')
+    }
+}
+
+let student = new Student(1, 'Jhon', 'john@gmail.com');
+
+//Method Overriding
+
+class Teacher extends Person {
+    override get fullName() { // We use OVERRIDE to say the compiler we are overiding the method
+        return 'Professor' + super.fullName; // We use the super keyword to access the method fullName from the father class
+    }
+}
+
+let teacher = new Teacher('Jhon', 'Smith');
+
+// Polymorphism
+// This means taking different forms
+
+class Principal extends Person {
+    override get fullName() {
+        return 'Principal' + super.fullName;
+    }
+}
+
+printNames([
+    new Student(1, 'John', 'Smith'),
+    new Teacher('Mosh', 'Hamedani'),
+])
+
+function printNames(people: Person[]) {
+    for( let person of people )
+        console.log(person.firstName);
+}
 
